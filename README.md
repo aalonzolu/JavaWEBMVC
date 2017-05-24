fee8fd7773f9549de2a58a4da7b1123cdb020c01
Andres Alonzo <andres.alonzo@proponiendosoluciones.com>
2017-05-23 22:55:34 -0600

    add webservice

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 TrainingMeServletsMVCRegistro
 # JavaWEBMVC
+
+## Base de datos
+- Crear base de datos
+- Configurar conexion a base de datos en el archivo Conexion en el paquete "modelos"
+- Importar archivo schema.sql en la base de datos
---
+++ schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `User` (
+  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idUser`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
